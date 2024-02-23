3a7e8dd78c51363e6a07bbc6eddf736a8982c888
suhyun96 <besthazu@gmail.com>
2024-02-23 21:33:24 +0900

    BuildContext에 대해서 정리 및 추가

--- code_factory/standard/splash_screen/lib/main.dart
+++ code_factory/standard/splash_screen/lib/main.dart
@@ -4,8 +4,7 @@ import 'package:splash_screen.dart/screen/splash_screen.dart';
 void main() {
   //화면 전체에 SplashScreen위젯을 그릴 거니까 runApp의 매개변수로 넣어준다
   runApp(const MaterialApp(
-    home: Scaffold(
-      body: SplashScreen(),
+    home: SplashScreen(),
     ),
-  ));
+  );
 }
